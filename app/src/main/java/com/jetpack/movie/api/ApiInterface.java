7b760fee1b2fbde424b6dfffb5402ab465f52e8a
gunawanasch <gunawan.aschari@gmail.com>
2020-01-11 21:48:47 +0700

    Update access api

--- app/src/main/java/com/jetpack/movie/api/ApiInterface.java
+++ app/src/main/java/com/jetpack/movie/api/ApiInterface.java
@@ -7,15 +7,15 @@ import retrofit2.http.Path;
 import retrofit2.http.Query;
 
 public interface ApiInterface {
-    @GET("3/movie/popular?api_key=d007adce10e29ffdfd6c4c9807e8c500&language=en-US")
+    @GET("3/movie/popular?api_key=api_key&language=en-US")
     Call<ResponseBody> getListMovie(@Query("page") String page);
 
-    @GET("3/movie/{id}?api_key=d007adce10e29ffdfd6c4c9807e8c500&language=en-US")
+    @GET("3/movie/{id}?api_key=api_key&language=en-US")
     Call<ResponseBody> getDetailMovie(@Path("id") String id);
 
-    @GET("3/tv/popular?api_key=d007adce10e29ffdfd6c4c9807e8c500&language=en-US")
+    @GET("3/tv/popular?api_key=api_key&language=en-US")
     Call<ResponseBody> getListTVShow(@Query("page") String page);
 
-    @GET("3/tv/{id}?api_key=d007adce10e29ffdfd6c4c9807e8c500&language=en-US")
+    @GET("3/tv/{id}?api_key=api_key&language=en-US")
     Call<ResponseBody> getDetailTVShow(@Path("id") String id);
 }
